5427056d79016c951f998cdffcdda182cd9de50d
HridayaK <55268471+hridaya0709@users.noreply.github.com>
2020-12-14 11:32:54 +0530

    Improved enhancement of game

--- Alien Invasion/scoreboard.py
+++ Alien Invasion/scoreboard.py
@@ -33,8 +33,10 @@ class Scoreboard():
             self.ships.add(ship)
 
     def prep_level(self):
-        """turn the level into a rendered image."""
-        self.level_image = self.font.render(str(self.stats.level),True,self.text_color,self.ai_settings.bg_color)
+        """Turn the level into a rendered image."""
+        level_str = "Level: " + str(self.stats.level)
+        self.level_image = self.font.render(level_str,True,self.text_color,self.ai_settings.bg_color)
+
 
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
@@ -44,7 +46,7 @@ class Scoreboard():
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "Highest: " + "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str,True,self.text_color,self.ai_settings.bg_color)
 
         # Center the high score at the top of the screen.
@@ -55,7 +57,7 @@ class Scoreboard():
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = int(round(self.stats.score, -1))
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Current: " + "{:,}".format(rounded_score)
         score_str = str(self.stats.score)
         self.score_image = self.font.render(score_str,True,self.text_color,self.ai_settings.bg_color)
 
